a7112e4774da8b155f7eda9b6892b9dbb04f5114
m-a-t-9 <57391957+m-a-t-9@users.noreply.github.com>
2020-05-11 20:07:28 +0200

    update DOOR.py

--- Core/DOOR.py
+++ Core/DOOR.py
@@ -1,4 +1,28 @@
+from Logger import *
+
+STATES = {"UNLOCKED": 0, "LOCKED": 1}
+AUTH_WAY = {0:"PIN", 1:"FINGERPRINT"}
+
 class DOOR:
   
-  def __init__(self):
+  __state = None
+  __authType = []
+  __psw = None
+  
+  def __init__(self, object):
+    self.lg = Logger("syslog.LOG", "DOOR")
+    self.__initialize(object)
+  
+  def __initialize(self, object):
+    for child in object:
+      if child.tag == "AUTH":
+        __authType.append(AUTHORIZATION(child))
+  
+  def getStates():
+    return STATES
+    
+  def __encrypt(self):
+    pass
+  
+  def __decrypt(self):
     pass
